bb11badba054d378629533298cdce34f1cb2ac5f
Moldovan Iulian <iulianmoldovan162@gmail.com>
2025-02-05 12:37:56 +0200

    added login and logout

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 # Create your views here.
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
+
+
     
 def signup(request):
     
@@ -40,3 +44,23 @@ def signup(request):
             
     else:
         return render(request, 'signup.html')    
+    
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+
+@login_required(login_url='signin')    
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
