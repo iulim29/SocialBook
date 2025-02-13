926e25bac6716e6e62261b2d8e9a179c94b5fc68
Moldovan Iulian <iulianmoldovan162@gmail.com>
2025-02-13 11:10:34 +0200

    added search option

--- social_book/core/urls.py
+++ social_book/core/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('', views.index, name='index'),     
+     path('', views.index, name='index'), 
+     path('search', views.search, name='search'),   
      path('signup', views.signup, name='signup'),
      path('signin', views.signin, name='signin'),
      path('logout', views.logout, name='logout'),
